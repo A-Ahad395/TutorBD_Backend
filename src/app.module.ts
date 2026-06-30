2f16fed84d483dff3f1c51990e2b33af52df1cae
roshninaznin <147531288+roshninaznin@users.noreply.github.com>
2026-07-01 04:02:06 +0600

    Added moderator module with image upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
 import { TutorModule } from './tutor/tutor.module';
 import { StudentModule } from './student/student.module';
+import { ModeratorModule } from './moderator/moderator.module';
+
 
 @Module({
-  imports: [AdminModule, TutorModule, StudentModule],
+  imports: [AdminModule, TutorModule, StudentModule, ModeratorModule],
   controllers: [AppController],
   providers: [AppService],
 })
